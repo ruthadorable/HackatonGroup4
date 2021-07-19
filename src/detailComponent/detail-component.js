d7537648b03fbc430a10029b1151acdb5b8a81d0
Isaac <mendez.vovinam@gmail.com>
2021-07-19 13:00:50 +0200

    detailComponent create

---
+++ src/detailComponent/detail-component.js
@@ -0,0 +1,15 @@
+  
+import React from 'react';
+import { Text, Image, View } from 'react-native';
+
+const DetailComponent = () => {
+
+    return (
+        <View>
+            <Text></Text>
+            <Image source={require()} />
+        </View>
+    )
+}
+
+export default DetailComponent;
