3f80d03e30b309ac8389af85be558faa5e4a0a3a
Isaac <mendez.vovinam@gmail.com>
2021-07-19 15:10:26 +0200

    detailComponent with stylesheet

--- src/detailComponent/detail-component.js
+++ src/detailComponent/detail-component.js
@@ -7,18 +7,48 @@ const DetailComponent = (props) => {
     const {title, comp, genre, synop, date, img, note} = props;
 
     return (
-        <View>
-            <Text>Titre: {title}</Text>
-            <Text>Compagnie de production: {comp}</Text>
-            <Text>Genre(s): {genre}</Text>
-            <Text>Synopsis: {synop}</Text>
-            <Text>Date de sortie: {date}</Text>
-            <Text>Affiche cinéma</Text>
-                <Image source={{uri: `https://image.tmdb.org/t/p/w500/${img}.jpg`}} />
-            <Text>Note: {note}</Text>
-            
+        <View style={style.mainLayout}>
+            <View style={style.infoLayout}>
+                <Text style={style.title}>Titre: {title}</Text>
+                <Text style={style.info}>Compagnie(s) de production: {comp}</Text>
+                <Text style={style.info}>Genre(s): {genre}</Text>
+                <Text style={style.synop}>Synopsis: {synop}</Text>
+                <Text style={style.info}>Date de sortie: {date}</Text>
+                <Text>Affiche cinéma</Text>
+                    <Image style={style.imgFilm} 
+                    source={{uri: `https://image.tmdb.org/t/p/w500/${img}.jpg`}} />
+                <Text style={style.info}>Note: {note}</Text>
+            </View>
         </View>
-    )
+    );
 }
+
+const style = StyleSheet.create({
+    mainLayout : {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    infoLayout: {
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 18
+    },
+    info: {
+        fontSize:14
+    },
+    synop: {
+        fontSize:12
+    },
+    imgFilm : {
+        width: 75, 
+        height: 100,
+        borderColor: '#000',
+        borderWidth: 1, 
+        borderRadius: 20
+    }
+})
+
 
 export default DetailComponent;
