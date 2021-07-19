d1b11eee5b5c268f5ec5f620b74292d0511c379b
Isaac <mendez.vovinam@gmail.com>
2021-07-19 14:21:02 +0200

    add componentdetail without style

--- src/detailComponent/detail-component.js
+++ src/detailComponent/detail-component.js
@@ -2,12 +2,21 @@
 import React from 'react';
 import { Text, Image, View } from 'react-native';
 
-const DetailComponent = () => {
+const DetailComponent = (props) => {
+
+    const {title, comp, genre, synop, date, img, note} = props;
 
     return (
         <View>
-            <Text></Text>
-            <Image source={require()} />
+            <Text>Titre: {title}</Text>
+            <Text>La compagnie de production: {comp}</Text>
+            <Text>Genre(s): {genre}</Text>
+            <Text>Synopsis: {synop}</Text>
+            <Text>Date de sortie: {date}</Text>
+            <Text>Affiche cinéma</Text>
+                <Image source={{uri: `https://image.tmdb.org/t/p/w500/${img}.jpg`}} />
+            <Text>Note: {note}</Text>
+            
         </View>
     )
 }
